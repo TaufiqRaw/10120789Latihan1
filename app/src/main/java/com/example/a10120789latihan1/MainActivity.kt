59527023cd4bfe95177a9001cbf8319f97f56b3d
taufiqraw <waskitaufiq@gmail.com>
2023-04-11 10:40:48 +0700

    Added Laporan kerja section.

--- app/src/main/java/com/example/a10120789latihan1/MainActivity.kt
+++ app/src/main/java/com/example/a10120789latihan1/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.a10120789latihan1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * Taufiq Ridho Adi Waskita
+ * 10120789
+ * IF-9
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
